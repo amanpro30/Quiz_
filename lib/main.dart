e6ba59a520609628508659ba63f9ec5acfb7feb8
amanpro30 <aman.k17@iiits.in>
2020-02-15 13:19:52 +0530

    added the styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,58 @@
 import 'package:flutter/material.dart';
 
+import 'question.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello!f'),);
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+class _MyAppState extends State<MyApp>{
+  var _questionIndex = 0;
+  void _answerQuestion(){
+    setState(() {
+      _questionIndex = _questionIndex + 1;  
+    });
+    print(_questionIndex);
+  }
+  Widget build(BuildContext context) {
+    var question = [
+      'what "s your favourite color ?',
+      'what "s your favourite animal ?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Question(question[_questionIndex]),
+            RaisedButton(
+              onPressed: _answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                // ....
+                print('Answer 2 chosen');
+              },
+              child: Text('Answer 2'),
+            ),
+            RaisedButton(
+              onPressed: _answerQuestion,
+              child: Text('Answer 3'),
+            ),
+            RaisedButton(
+              onPressed: _answerQuestion,
+              child: Text('Answer 4'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
